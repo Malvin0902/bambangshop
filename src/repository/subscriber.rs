754e73666ddd2a2694e6d20e9f2f88ccbf582403
Malv <malvinmraqin@gmail.com>
2025-03-27 02:09:34 +0700

    Implement delete function in Subscriber repository

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -20,6 +20,7 @@ impl SubscriberRepository {
             .insert(subscribe_value.url.clone(), subscribe_value);
         return subscriber
     }
+    
     pub fn list_all(product_type: &str) -> Vec<Subscriber> {
         if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
@@ -27,5 +28,17 @@ impl SubscriberRepository {
 
         return SUBSCRIBERS.get(product_type).unwrap().iter()
             .map(|f| f.value().clone()).collect();
+    }
+
+    pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+        let result = SUBSCRIBERS.get(product_type).unwrap()
+            .remove(url);
+        if !result.is_none() {
+            return Some(result.unwrap().1);
+        }
+        return None;
     }
 }
